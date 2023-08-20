83ff87682a69d8bd3cf318cd18a419c88f7ecaa2
GASgod1111 <reagan.peter@student.moringaschool.com>
2023-08-20 11:17:54 +0300

    Strings

---
+++ Strings.py
@@ -0,0 +1,42 @@
+#creating a String 
+#with single quotes
+String1 ='Welcome to the Geeks World'
+print("String with the use of Single quotes: ")
+print(String1)
+ 
+#with a double quotes
+String1= "I'm a Geek"
+print("\nString with the use of Double quotes: ")
+print(String1)
+print(type(String1))
+
+# a double quotes
+String1='''I'm a Geek and I live a world of "Geeks"'''
+print("\nString with the use of Triple Quotes: ")
+print (String1)
+print(type(String1))
+
+# creating a string with a double quotes
+#quotes allows multiple lines
+String1= '''Geeks
+             for
+             life'''
+print("\nString with the use of Triple Quotes: ")
+print(String1)
+
+#Python Program to Access
+#characters of String
+
+String1= "GeeksForGeeks"
+print("Initial String: ")
+print(String1)
+
+#printing First character
+#Accessing elements of String
+# Strings can be accessed by using the method of Indexing
+print("\nFirst character of String is: ")
+print(String1[0])\
+
+# printing 
+print("\nLast character of the String is : ")
+print(String1[-1])
